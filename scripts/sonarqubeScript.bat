5fe83391d26c3786ea9d6a1f4b1257cf27fe9ee3
Daniel Kurta <s64159@beuth-hochschule.de>
2019-02-10 18:02:39 +0100

    add sonarqubeScript and connect it to jenkins pipeline

--- scripts/sonarqubeScript.bat
+++ scripts/sonarqubeScript.bat
@@ -1,1 +1,2 @@
+cd C:\Program Files (x86)\Jenkins\workspace\ChessPetProject
 C:\Users\Admin\Documents\sonar-scanner-cli-3.3.0.1492-windows\sonar-scanner-3.3.0.1492-windows\bin\sonar-scanner.bat -D"sonar.projectKey=dkurta_chess_pet_project" -D"sonar.organization=dkurta-github" -D"sonar.sources=."-D"sonar.host.url=https://sonarcloud.io" -D"sonar.login=83e015eb467afd1ccfe234a5f77c5631575bed0b"
