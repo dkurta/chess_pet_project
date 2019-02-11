1d15733162f8e3f4fccd3946ced2aef79de373cc
Daniel Kurta <s64159@beuth-hochschule.de>
2019-02-11 01:36:21 +0100

    added Docstring and refactoring

--- chess_src/Main.py
+++ chess_src/Main.py
@@ -1,15 +1,9 @@
+"""Entrypoint for the Chess Pet Project. Instanciates a Game (starting from a fen position is also possible)
+and calls the play() method."""
+
 from chess_src.Game import Game
-from chess_src.MoveDSL import translate
 
 if __name__ == '__main__':
-    # g = Game(fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1')
-    # g = Game(fen='rnbqkb1r/pppppppp/7n/8/4P1P1/8/PPPP1P1P/RNBQKBNR b KQkq - 0 1')
-    # g = Game(fen="r5k1/pq1nR1pp/1p3r2/5p2/3P4/3B1b1Q/P4PPP/R5K1 w - - 0 1")
-    # s = translate('Springer nach d6')
-    # print(s)
-    while True:
-        move = input('Please enter your move:')
-        s = translate(move)
-        print(s)
+    # g = Game(fen='rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1') # example for a specific position in fen format
     g = Game()
     g.play()
